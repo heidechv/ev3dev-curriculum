19183cfb4ed10d6ee73d167e01c55db9024c6ef5
arcots <arcots@rose-hulman.edu>
2018-01-31 10:35:02 -0500

    Changed TODO to done

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -29,3 +29,13 @@ class Snatch3r(object):
         degrees = inches_target * 90
         self.left_motor.run_to_rel_pos(speed_sp=speed_deg_per_second, position_sp=degrees, stop_action='brake')
         self.right_motor.run_to_rel_pos(speed_sp=speed_deg_per_second, position_sp=degrees, stop_action='brake')
+
+    def turn_degrees(self, degrees_to_turn, speed_deg_per_second):
+        if degrees_to_turn < 0:
+            self.right_motor.run_to_rel_pos(speed_sp = -speed_deg_per_second, position_sp = -degrees_to_turn)
+            self.left_motor.run_to_rel_pos(speed_sp = speed_deg_per_second, position_sp = -degrees_to_turn)
+        else:
+            self.right_motor.run_to_rel_pos(speed_sp = speed_deg_per_second, position_sp = degrees_to_turn)
+            self.left_motor.run_to_rel_pos(speed_sp = speed_deg_per_second, position_sp = degrees_to_turn)
+        self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
+        self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
