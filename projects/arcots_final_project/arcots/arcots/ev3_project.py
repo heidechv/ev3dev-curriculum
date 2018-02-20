fe41a071f54dce2525382b595aaf3d890bfaf411
arcots <arcots@rose-hulman.edu>
2018-02-19 23:49:30 -0500

    updating

--- projects/arcots_final_project/arcots/arcots/ev3_project.py
+++ projects/arcots_final_project/arcots/arcots/ev3_project.py
@@ -1,15 +1,17 @@
-'Satya Arcot'
-"EV3 Final Project CSSE120"
-'CSSE120 Session 2 Dr. Fisher'
+#'Satya Arcot'
+#"EV3 Final Project CSSE120"
+#'CSSE120 Session 2 Dr. Fisher'
+
 
 import ev3dev.ev3 as ev3
-import time
 import robot_controller as robo
 import mqtt_remote_method_calls as com
+
 
 class Delegate(object):
     def __init__(self, robot):
         self.robot = robot
+
     def chase_the_ball(self):
         print('Chase the ball')
         ev3.Sound.speak('Time for some cardio').wait()
@@ -25,35 +27,34 @@ class Delegate(object):
                 ev3.Sound.speak('I am tired')
 
 
-robot = robo.Snatch3r()
-delegate = Delegate(robot)
-mqtt_client = com.MqttClient(delegate)
-mqtt_client.connect_to_pc()
-
-
 """The robot will spin to find and track an object in front of the pixy camera"""
-def spin_for_the_ball(self, robo):
-    ev3.Sound.speak('Time to work my obliques').wait()
-    while True:
-        pixy_x = robo.get_pixy_x()
-        print(pixy_x)
-        if pixy_x is None:
-            robot.drive(30, -30)
-        elif pixy_x < 140:
-            robot.drive(-30, 30)
-        elif pixy_x > 170:
-            robot.drive(30, -30)
-        else:
-            robot.stop()
-            ev3.Sound.speak('I am tired')
+# def spin_for_the_ball(self, robo):
+#     ev3.Sound.speak('Time to work my obliques').wait()
+#     while True:
+#         pixy_x = robo.get_pixy_x()
+#         print(pixy_x)
+#         if pixy_x is None:
+#             robot.drive(30, -30)
+#         elif pixy_x < 140:
+#             robot.drive(-30, 30)
+#         elif pixy_x > 170:
+#             robot.drive(30, -30)
+#         else:
+#             robot.stop()
+#             ev3.Sound.speak('I am tired')
 
 
 """ The robot will display through an mqtt client  """
+
 
 def main():
     print('--------------------')
     print('Chasing the carrot')
     print('--------------------')
+    robot = robo.Snatch3r()
+    delegate = Delegate(robot)
+    mqtt_client = com.MqttClient(delegate)
+    mqtt_client.connect_to_pc()
     ev3.Sound.speak('I want the carrot').wait()
     robot.loop_forever()
 
--- projects/arcots_final_project/arcots/arcots/pc_project.py
+++ projects/arcots_final_project/arcots/arcots/pc_project.py
@@ -1,11 +1,15 @@
 import tkinter
 from tkinter import ttk
 import mqtt_remote_method_calls as com
-import robot_controller as robo
-import ev3dev.ev3 as ev3
 
-def start_program(mqtt_client):
-    mqtt_client.open()
+
+def press(mqtt_client):
+    print('arrive')
+    mqtt_client.send_message('chase_the_ball')
+
+
+# def start_program(mqtt_client):
+#     mqtt_client.open()
 
 
 def quit_program(mqtt_client):
@@ -13,28 +17,29 @@ def quit_program(mqtt_client):
     exit()
 
 
-robot = robo.Snatch3r()
+def main():
+    mqtt_client = com.MqttClient()
+    mqtt_client.connect_to_ev3()
+
+    root = tkinter.Tk()
+    root.title = "Master Controller"
+
+    main_frame = ttk.Frame(root, padding=5)
+    main_frame.grid()
+
+    quit_button = ttk.Button(main_frame, text="Quit")
+    quit_button.grid(row=3, column=1)
+    quit_button["command"] = lambda: quit_program(mqtt_client)
+
+    spin_button = ttk.Button(main_frame, text="Spin")
+    spin_button.grid(row=3, column=2)
+    spin_button["command"] = lambda: mqtt_client.send_message('spin_for_the_ball')
+
+    chase_button = ttk.Button(main_frame,  text="Chase")
+    chase_button.grid(row=3, column=3)
+    chase_button["command"] = lambda: press(mqtt_client)
+
+    root.mainloop()
 
 
-mqtt_client = com.MqttClient()
-mqtt_client.connect_to_ev3()
-
-root = tkinter.Tk()
-root.title = "Master Controller"
-
-main_frame = ttk.Frame(root, padding=5)
-main_frame.grid()
-
-quit_button = ttk.Button(main_frame, text="Quit")
-quit_button.grid(row=3, column=1)
-quit_button["command"] = lambda: quit_program(mqtt_client)
-
-spin_button = ttk.Button(main_frame, text="Spin")
-spin_button.grid(row=3, column=2)
-spin_button["command"] = lambda: mqtt_client.send_message('spin_for_the_ball')
-
-chase_button = ttk.Button(main_frame,  text="Chase")
-chase_button.grid(row=3, column=3)
-chase_button["command"] = lambda: mqtt_client.send_message('chase_the_ball')
-
-root.mainloop()
+main()
