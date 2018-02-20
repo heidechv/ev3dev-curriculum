326bc5ccdad2dec3d1d840e04b1b3d76cd213f73
arcots <arcots@rose-hulman.edu>
2018-02-19 19:47:25 -0500

    Booyah! mqtt works.

--- projects/projects/arcots/arcots/pc_project.py
+++ projects/projects/arcots/arcots/pc_project.py
@@ -2,21 +2,24 @@ import tkinter
 from tkinter import ttk
 import mqtt_remote_method_calls as com
 import robot_controller as robo
-
+import ev3dev.ev3 as ev3
 
 def start_program(mqtt_client):
     mqtt_client.open()
 
 
 def quit_program(mqtt_client):
-    mqtt_client.close()
+    mqtt_client.send_message('')
     exit()
+
+
+robot = robo.Snatch3r()
 
 
 mqtt_client = com.MqttClient()
 mqtt_client.connect_to_ev3()
 
-root = ttk.Frame
+root = tkinter.Tk()
 root.title = "Master Controller"
 
 main_frame = ttk.Frame(root, padding=5)
@@ -33,10 +36,41 @@ quit_button["command"] = lambda: quit_program(mqtt_client)
 
 spin_button = ttk.Button(main_frame, text="Spin")
 spin_button.grid(row=3, column=2)
-spin_button["command"] = lambda: spin_for_the_ball(robot)
+spin_button["command"] = lambda: ['spin_for_the_ball', [robot]]
 
 chase_button = ttk.Button(main_frame,  text="Chase")
 chase_button.grid(row=3, column=3)
-chase_button["command"] = lambda: chase_the_ball()
+chase_button["command"] = lambda: ['chase_the_ball', [robot]]
 
-robot = robo.Snatch3r()
+root.mainloop()
+
+
+def spin_for_the_ball(self, robo):
+    ev3.Sound.speak('Time to work my obliques').wait()
+    while True:
+        pixy_x = robo.get_pixy_x()
+        print(pixy_x)
+        if pixy_x is None:
+            robo.drive(30, -30)
+        elif pixy_x < 140:
+            robot.drive(-30, 30)
+        elif pixy_x > 170:
+            robot.drive(30, -30)
+        else:
+            robot.stop()
+            ev3.Sound.speak('I am tired')
+
+
+def chase_the_ball(robot):
+    ev3.Sound.speak('Time for some cardio').wait()
+    while True:
+        front_sensor = robot.get_front_proximity_sensor_reading()
+        print(front_sensor)
+        if front_sensor < 420:
+            robot.start_moving(40, 40)
+        elif front_sensor > 550:
+            robot.stop_moving()
+        else:
+            robot.stop_moving()
+            ev3.Sound.speak('I am tired')
+
--- projects/projects/arcots/ev3_project.py
+++ projects/projects/arcots/ev3_project.py
@@ -5,41 +5,41 @@
 import ev3dev.ev3 as ev3
 import time
 import robot_controller as robo
+import mqtt_remote_method_calls as com
 
 
+robot = robo.Snatch3r()
+mqtt_client = com.MqttClient(robot)
+mqtt_client.connect_to_pc()
 
-class My_delegate(object):
-    def __init__(self):
-        self.running = True
 
+def spin_for_the_ball(self, robo):
+    ev3.Sound.speak('Time to work my obliques').wait()
+    while True:
+        pixy_x = robo.get_pixy_x()
+        print(pixy_x)
+        if pixy_x is None:
+            robo.drive(30, -30)
+        elif pixy_x < 140:
+            robot.start_moving(-30, 30)
+        elif pixy_x > 170:
+            robot.start_moving(30, -30)
+        else:
+            robot.stop_moving()
+            ev3.Sound.speak('I am tired')
 
-    def spin_for_the_ball(robot):
-        ev3.Sound.speak('Time to work my obliques').wait()
-        while True:
-            pixy_x = robot.get_pixy_x()
-            print(pixy_x)
-            if pixy_x is None:
-                robot.start_moving(30, -30)
-            elif pixy_x < 140:
-                robot.start_moving(-30, 30)
-            elif pixy_x > 170:
-                robot.start_moving(30, -30)
-            else:
-                robot.stop_moving()
-                ev3.Sound.speak('I am tired')
-
-    def chase_the_ball(robot):
-        ev3.Sound.speak('Time for some cardio').wait()
-        while True:
-            front_sensor = robot.get_front_proximity_sensor_reading()
-            print(front_sensor)
-            if front_sensor < 420:
-                robot.start_moving(40, 40)
-            elif front_sensor > 550:
-                robot.stop_moving()
-            else:
-                robot.stop_moving()
-                ev3.Sound.speak('I am tired')
+def chase_the_ball(robot):
+    ev3.Sound.speak('Time for some cardio').wait()
+    while True:
+        front_sensor = robot.get_front_proximity_sensor_reading()
+        print(front_sensor)
+        if front_sensor < 420:
+            robot.start_moving(40, 40)
+        elif front_sensor > 550:
+            robot.stop_moving()
+        else:
+            robot.stop_moving()
+            ev3.Sound.speak('I am tired')
 
 
 """ The robot will display through an mqtt client  """
